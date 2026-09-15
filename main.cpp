7d053a3b7ea7d3140125be820f16372cd1ff787a
unknown <ritsu070709@gmail.com>
2026-09-15 20:52:22 +0300

    m

--- main.cpp
+++ main.cpp
@@ -1,1 +1,10 @@
+#include <iostream>
+int main(){
+    std::cout<<"Hello World"<<std::endl;
+}
 
+std::string name;
+    std::cout<<"Enter your name: ";
+    std::cin>>name;
+    std::cout<<"Hello, "<<name<<"Ch z chto pisat ."<<std::endl;
+}
