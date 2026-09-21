5523fd2525d3c6f312e321b80f2a2b63c0280ec0
lona <ritsu07072009@gmail.com>
2026-09-21 13:47:36 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,10 +1,3 @@
-#include <iostream>
-int main(){
-    std::cout<<"Hello World"<<std::endl;
-}
-
-std::string name;
-    std::cout<<"Enter your name: ";
-    std::cin>>name;
-    std::cout<<"Hello, "<<name<<"Ch z chto pisat ."<<std::endl;
+void hello() {
+    cout << "Hello!";
 }
